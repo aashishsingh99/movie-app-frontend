2fbc9e60462de69bb74248d57863232619d811c9
Sahil Aggarwal <sahil.aggarwal@hyperverge.co>
2021-01-25 17:23:17 +0530

    React setupp, Added Register and Login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-
+import React, { Fragment,useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import setAuthToken from './utils/setAuthToken';
+import store from './store';
+import Alert from './containers/layout/Alert';
+import Navbar from './containers/layout/Navbar';
+import Landing from './containers/layout/Landing';
+import Login from './containers/auth/Login';
+import Register from './containers/auth/Register';
 import './App.css';
 
-function App() {
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar> </Navbar>
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert/>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+    </Provider>
   );
-}
-
+};
 export default App;
---
+++ src/actions/auth.js
@@ -0,0 +1,104 @@
+// import api from '../utils/api';
+import axios from 'axios';
+
+import { setAlert } from './alert';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_PROFILE,
+} from './types';
+
+// Load User
+export const loadUser = () => async dispatch => {
+  try {
+    // const res = await api.get('/auth');
+    const res = { data: 'hi' };
+    console.log('load_user()', res.data);
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log('Load_use()', err);
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
+
+// Register User
+export const register = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  // const body = JSON.stringify({name,email,password,role})
+  // console.log(body.name)
+  try {
+    // const res = await api.post('/users', formData);
+    //const res1 = await api.post('/profile', profileData);
+    const res = await axios.post('/api/users', formData, config);
+    // const res={data:'hi'}
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
+};
+
+// Login User
+export const login = (body) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  
+
+  try {
+    // const res = await api.post('/auth', body);
+    console.log('hi')
+    const res = await axios.post('/api/auth', body, config);
+
+    // const res={data:'hi'}
+    console.log(res.data)
+    await dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    console.log(err);
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+};
+
+// Logout
+export const logout = () => ({ type: CLEAR_PROFILE, type: LOGOUT });
